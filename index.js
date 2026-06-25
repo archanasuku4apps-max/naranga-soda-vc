f94fb228a38cbe40d21ebbfe5e9245213cc75169
Luna <archanasuku4apps@gmail.com>
2026-06-25 12:43:36 +0530

    add express server

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Naranga Soda VC Bot Online");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Web server running");
+});
 const {
   Client,
   GatewayIntentBits,
